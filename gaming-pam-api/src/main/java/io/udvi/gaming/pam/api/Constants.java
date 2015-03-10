652f4b9a3efa458b9b43facffe42874ff44dca39
Suresh Reddy Guntaka <sureshreddy.guntaka@outlook.com>
2015-03-11 05:10:54 +0530

    Just started working

---
+++ gaming-pam-api/src/main/java/io/udvi/gaming/pam/api/Constants.java
@@ -0,0 +1,7 @@
+package io.udvi.gaming.pam.api;
+
+/**
+ * Created by sureshreddyguntaka on 11/03/15.
+ */
+public interface Constants {
+}
---
+++ gaming-pam-api/src/main/java/io/udvi/gaming/pam/api/model/Player.java
@@ -0,0 +1,9 @@
+package io.udvi.gaming.pam.api.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by sureshreddyguntaka on 11/03/15.
+ */
+public class Player implements Serializable{
+}
